acfd492f62b67d654ececde5f9a4d193f877c93f
wwang <wwang@wxsmartech.cn>
2022-06-28 14:23:50 +0800

    测试2.0 测试版本迭代

--- actionLibrary/src/main/java/com/sdk/actionlibrary/ActionUtils.java
+++ actionLibrary/src/main/java/com/sdk/actionlibrary/ActionUtils.java
@@ -7,4 +7,8 @@ public class ActionUtils {
     public static void actionSwitch(){
         Log.i("ActionUtils","测试调用actionSwitch()");
     }
+
+    public static void testUPVersion(){
+        Log.i("ActionUtils","测试版本更新1.0.2才有");
+    }
 }
